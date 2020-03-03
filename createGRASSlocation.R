a2ce34e33dd161916f3ace7dfe89341e4fa13500
Vaclav Petras <wenzeslaus@gmail.com>
2020-03-02 21:56:27 -0500

    Document the create loc fun and fix EPSG case
    Documentation of purpose and paramters of the create location function.
    It also fixes a bug in the system call in the EPSG case where EPSG was
    missing from the parameters (and thus failing).

--- createGRASSlocation.R
+++ createGRASSlocation.R
@@ -1,8 +1,18 @@
+#' Create GRASS GIS location
+#'
+#' Calls GRASS GIS to create a new GRASS GIS location using either a
+#' georeferenced file or EPSG code. The parent directories of the
+#' provided *locationPath* need to exist.
+#'
+#' @param grassExecutable GRASS GIS executable (full path or command)
+#' @param readProjectionFrom A geospatial file with CRS to use
+#' @param EPSG EPSG code of a desired CRS
+#' @param locationPath Full path to the new created location directory
 createGRASSlocation <- function (grassExecutable, readProjectionFrom, EPSG, locationPath) {
   if (missing(EPSG)){
     system(paste("\"", grassExecutable, "\"", " -c ", "\"", readProjectionFrom, "\"", " ", "\"", locationPath, "\"", " -e ", sep = ""))
   }
   else{
-    system(paste("\"", grassExecutable, "\"", " -c ", "EPSG:", " ", "\"", locationPath, "\"", " -e ", sep = ""))
+    system(paste("\"", grassExecutable, "\"", " -c ", "EPSG:", EPSG, " ", "\"", locationPath, "\"", " -e ", sep = ""))
   }
 }
